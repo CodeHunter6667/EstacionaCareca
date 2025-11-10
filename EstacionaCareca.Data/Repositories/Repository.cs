6c8506a188f85ea95863288c271f204a34ee5e97
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-10 20:23:55 -0300

    feat: adicionado classes de repositório

---
+++ EstacionaCareca.Data/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace EstacionaCareca.Data.Repositories;
+
+public class Repository<T>(SqlConnection connection) where T : class
+{
+    public IEnumerable<T> Get()
+        => connection.GetAll<T>();
+
+    public T GetById(int id)
+        => connection.Get<T>(id);
+
+    public void Create(T model)
+        => connection.Insert(model);
+
+    public void Update(T model)
+    {
+        var idProperty = typeof(T).GetProperty("Id");
+        if (idProperty != null && (int)idProperty.GetValue(model) != 0)
+        {
+            connection.Update(model);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        var model = connection.Get<T>(id);
+        connection.Delete(model);
+    }
+
+    public void Delete(T model)
+        => connection.Delete(model);
+}
---
+++ EstacionaCareca.Data/Repositories/VeiculoRepository.cs
@@ -0,0 +1,11 @@
+﻿using EstacionaCareca.Shared.Models;
+using Microsoft.Data.SqlClient;
+
+namespace EstacionaCareca.Data.Repositories;
+
+public class VeiculoRepository : Repository<Veiculo>
+{
+    public VeiculoRepository(SqlConnection connection) : base(connection)
+    {
+    }
+}
