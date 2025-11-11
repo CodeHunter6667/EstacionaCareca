42a082942dd857af666c4b2760be6db92d1e97e9
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-10 21:30:42 -0300

    feat: adicionado Serviço

--- EstacionaCareca.Shared/Models/Veiculo.cs
+++ EstacionaCareca.Shared/Models/Veiculo.cs
@@ -6,5 +6,6 @@ public class Veiculo
     public string Placa { get; set; } = string.Empty;
     public DateTime DataEntrada { get; set; }
     public DateTime DataSaida { get; set; }
+    public decimal ValorPago { get; set; }
     public bool VeiculoSaiu { get; set; }
 }
