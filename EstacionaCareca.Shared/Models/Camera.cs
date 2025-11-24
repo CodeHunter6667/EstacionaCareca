4c1805ca8b92cd4bd0c54647312297d0accd3d4d
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-24 20:46:45 -0300

    feat: variavel de coordenador

--- EstacionaCareca.Shared/Models/Camera.cs
+++ EstacionaCareca.Shared/Models/Camera.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EstacionaCareca.Shared.Models
+﻿namespace EstacionaCareca.Shared.Models
 {
     public class Camera
     {
@@ -12,6 +6,6 @@ namespace EstacionaCareca.Shared.Models
         public string Nome { get; set; } = string.Empty;
         public string DiretorioImagens { get; set; } = string.Empty;
         public bool Ativa { get; set; } = true;
-
+        public bool Coordenador { get; set; }
     }
 }
